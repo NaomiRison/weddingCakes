3de1c4219ad41c0df83467113e7359bd317714bb
NaomiRison <nr.rison@gmail.com>
2016-12-10 21:44:14 -0500

    1 page website for user to pick wedding cake and leave contact info

---
+++ project1.js
@@ -0,0 +1,10 @@
+$.stellar();
+
+$(document).ready(function (){
+	// when you click menu icon, it hides or show nav button
+	$(".menu-icon").click(function (){
+		// alert("hi");  test to check if it works
+		$("nav").slideToggle();
+	});
+ });
+
