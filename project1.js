cb44cae7e923c0972f1d7334bdcd538918382647
NaomiRison <nr.rison@gmail.com>
2017-01-11 16:05:48 -0500

    fix size of pictures

--- project1.js
+++ project1.js
@@ -2,9 +2,10 @@ $.stellar();
 
 // $(document).ready(function (){
 // 	// when you click menu icon, it hides or show nav button
-// 	$(".navbar-toggle").click(function (){
-// 		// alert("hi");  test to check if it works
-// 		$(".navbar-default").slideToggle();
+// 	$("button").click(function (){
+// 		alert("hi");  
+// 		$("#menu").slideToggle("ul");
+
 // 	});
 //  });
 
